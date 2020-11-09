10172c9882170cc3d74f8a78a141c635d638ec80
JessiG <amantalath@hotmail.com>
2020-11-09 11:40:06 +0100

    ani

--- AniPlay/ViewController.swift
+++ AniPlay/ViewController.swift
@@ -6,19 +6,53 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        print("view loaded")
-
         
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        
+        audioPlayer.play()
+        
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "bgmusic", ofType: "mp3")!))
+        }
+        catch{
+            print(error)
+            
+        }
+        
+        audioPlayer.play()
+
     }
+    
+    @IBAction func ClickPlay(_ sender: UIButton) {
+        let soundURL = NSURL(fileURLWithPath: Bundle.main.path(forResource: "yay", ofType: "wav")!)
 
+                do{
+                    audioPlayer = try AVAudioPlayer(contentsOf: soundURL as URL)
 
+                }catch {
+                    print("there was some error. The error was \(error)")
+                }
+                audioPlayer.play()
+
+            }
+    }
     
     
-}
+    
+
 
--- AniPlay/ViewController2.swift
+++ AniPlay/ViewController2.swift
@@ -6,25 +6,63 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController2: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
+
+
+    @IBOutlet weak var Button: UIButton!
+    
+    @IBOutlet weak var Correct: UILabel!
+    
+    var animals = [String]()
+    
+    var correct = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "bgmusic", ofType: "mp3")!))
+        }
+        catch{
+            print(error)
+            
+        }
+        audioPlayer.play()
 
-        // Do any additional setup after loading the view.
+        
+    
+        animals.append("Monkey")
+        animals.append("Horse")
+        animals.append("Cat")
+        animals.append("Rabbit")
+        
+        correct = animals.randomElement()!
+     
+        
+        
+        //thelabel.text = correct
+   
+  }
+    @IBAction func Button(_ sender: Any) {
+        Correct.text = "1"
     }
+    @IBAction func Button2(_ sender: Any) {
+        Correct.text = "2"
+    }
+    @IBAction func Button3(_ sender: Any) {
+        Correct.text = "3"
+    }
+    @IBAction func Button4(_ sender: Any) {
+        Correct.text = "4"
+    }
+    
+    
 
-    @IBAction func ClickWrong(_ sender: UIButton) {
-        print("Wrong")
-        sender.backgroundColor = UIColor.red
-    }
 }
-    
-    
-    
-    
-    
-
-
 
